f8c85b48ee07cfed27ee3a2492c0cf748df14ec6
thirikandanathan-sivaraj <thirishcloud@gmail.com>
2025-06-03 14:36:08 +0530

    Update outputs.tf

--- terraform/modules/alb/outputs.tf
+++ terraform/modules/alb/outputs.tf
@@ -1,1 +1,12 @@
+output "alb_dns_name" {
+  value = aws_lb.this.dns_name
+}
+
+output "target_group_arn" {
+  value = aws_lb_target_group.this.arn
+}
+
+output "listener_arn" {
+  value = aws_lb_listener.http.arn
+}
 
