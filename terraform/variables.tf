e6e30442c36303478091efe88e548a26305e44bf
thirikandanathan-sivaraj <thirishcloud@gmail.com>
2025-06-03 14:45:51 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,30 @@
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+  default     = "us-east-1"
+}
+
+variable "project_name" {
+  description = "Prefix for resource names"
+  default     = "eutech-assessment"
+}
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+  default = "10.0.1.0/24"
+}
+
+variable "private_subnet_cidr" {
+  default = "10.0.2.0/24"
+}
+
+variable "availability_zone" {
+  default = "us-east-1a"
+}
+
+variable "image_url" {
+  description = "Docker image URL to deploy"
+}
 
