48c4a980ce75ef584827d70ace55fc9826efd2c7
thirikandanathan-sivaraj <thirishcloud@gmail.com>
2025-06-03 14:25:13 +0530

    Update main.tf

--- terraform/modules/ecs/main.tf
+++ terraform/modules/ecs/main.tf
@@ -1,1 +1,49 @@
+resource "aws_ecs_cluster" "main" {
+  name = "${var.project_name}-cluster"
+}
+
+resource "aws_ecs_task_definition" "app" {
+  family                   = "${var.project_name}-task"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = "256"
+  memory                   = "512"
+  execution_role_arn       = var.execution_role_arn
+
+  container_definitions = jsonencode([
+    {
+      name      = "app"
+      image     = var.image_url
+      essential = true
+      portMappings = [
+        {
+          containerPort = 80
+          hostPort      = 80
+        }
+      ]
+    }
+  ])
+}
+
+resource "aws_ecs_service" "app" {
+  name            = "${var.project_name}-service"
+  cluster         = aws_ecs_cluster.main.id
+  task_definition = aws_ecs_task_definition.app.arn
+  launch_type     = "FARGATE"
+  desired_count   = 1
+
+  network_configuration {
+    subnets         = var.private_subnet_ids
+    assign_public_ip = false
+    security_groups  = [var.service_sg_id]
+  }
+
+  load_balancer {
+    target_group_arn = var.target_group_arn
+    container_name   = "app"
+    container_port   = 80
+  }
+
+  depends_on = [var.alb_listener]
+}
 
