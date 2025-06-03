8f45f34425e57755c398410a537d884e78919f5e
thirikandanathan-sivaraj <thirishcloud@gmail.com>
2025-06-03 14:35:44 +0530

    Update variables.tf

--- terraform/modules/alb/variables.tf
+++ terraform/modules/alb/variables.tf
@@ -1,1 +1,20 @@
+variable "project_name" {
+  description = "Prefix for resource names"
+  type        = string
+}
+
+variable "public_subnet_ids" {
+  description = "List of public subnet IDs"
+  type        = list(string)
+}
+
+variable "vpc_id" {
+  description = "ID of the VPC"
+  type        = string
+}
+
+variable "alb_sg_id" {
+  description = "Security group for the ALB"
+  type        = string
+}
 
