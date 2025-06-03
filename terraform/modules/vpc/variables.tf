23d01b538de073269f82ebbb73e1ed5250af4147
thirikandanathan-sivaraj <thirishcloud@gmail.com>
2025-06-03 14:12:41 +0530

    Update variables.tf

--- terraform/modules/vpc/variables.tf
+++ terraform/modules/vpc/variables.tf
@@ -1,1 +1,25 @@
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "public_subnet_cidr" {
+  description = "CIDR block for the public subnet"
+  type        = string
+}
+
+variable "private_subnet_cidr" {
+  description = "CIDR block for the private subnet"
+  type        = string
+}
+
+variable "az" {
+  description = "Availability zone"
+  type        = string
+}
+
+variable "project_name" {
+  description = "Name prefix for resources"
+  type        = string
+}
 
